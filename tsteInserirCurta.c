ca10d5c6b90724d19f4522bce07942e97a6af3a8
Ricardo Gontarz <105812609+EleelW@users.noreply.github.com>
2023-05-22 18:00:54 -0300

    Update tsteInserirCurta.c

--- tsteInserirCurta.c
+++ tsteInserirCurta.c
@@ -42,14 +42,29 @@ void imprimir(NO* raiz){
         imprimir(raiz->direita);
     }
 }
+int buscar(NO* raiz, int chave){
+    if(raiz == NULL){
+        return -1;
+    }else{
+        if(raiz->conteudo == chave){
+            return raiz->conteudo;
+        }else{
+            if(chave < raiz->conteudo){
+                return buscar(raiz->esquerda, chave);
+            }else{
+                return buscar(raiz->direita, chave);
+            }
 
+        }
+    }
+}
 int main(){
     int op, valor;
     ArvB arv;
     arv.raiz = NULL;
 
     do {
-        printf("\n0 - sair\n1 - inserir\n2 - imprimir\n");
+        printf("\n0 - sair\n1 - inserir\n2 - imprimir\n3 - Buscar\n");
         scanf(" %d", &op);
 
         switch (op) {
@@ -66,6 +81,11 @@ int main(){
                 imprimir(arv.raiz);
                 printf("\n");
                 printf("Tamanho: %d\n", tamanho(arv.raiz));
+                break;
+            case 3:
+                printf("Qual valor deseja buscar? ");
+                scanf(" %d", &valor);
+                printf("\nResultado: %d\n", buscar(arv.raiz, valor));
                 break;
             default:
                 printf("\nOpcao invalida...\n");
